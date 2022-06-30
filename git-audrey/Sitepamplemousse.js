3b4204ac730f59bc074d788ccfa1c0e80da42c8e
Audrey Cannesson <you@example.coma.cannesson5@gmail.comgit config --global user.name Your>
2022-06-30 18:48:20 +0200

    Ajout numero 2

--- git-audrey/Sitepamplemousse.js
+++ git-audrey/Sitepamplemousse.js
@@ -1,8 +1,11 @@
+// Animation de la frise
+
 const percentLabel = document.querySelector("#percent");
 const originalTitle = document.title;
 const frise = document.querySelector(".frise");
 let oldpercentage=0;
-let translation = -1;
+let translation = -0.1;
+
 window.addEventListener("scroll", () => {
   let scrollTop = window.scrollY;
   let docHeight = document.body.offsetHeight;
@@ -15,46 +18,45 @@ window.addEventListener("scroll", () => {
 
   translation ++;
   frise.setAttribute('style','transform:translateX('+translation+'%);') ;
-  }
+  } 
+
+  if (oldpercentage < scrollPercentRounded){
+    console.log(scrollPercentRounded,translation,frise);
+  
+    translation --;
+    frise.setAttribute('style','transform:translateX('+translation+'%);') ;
+    }
+
   oldpercentage = scrollPercentRounded;
+
 });
 
-  // for (scrollPercentRounded = 17; scrollPercentRounded < 40; scrollPercentRounded++) {
 
-  // }
-  //   }
 
-// window.addEventListener("scroll", () => {
-//   if(window.scrollY >= 800) {
-//     frise.classList.add("scroll");
-//     }
-//    });
+// Animation paiment
 
-// while (scrollPercentRounded >= 17 && scrollPercentRounded <= 39) {
-//  frise.style.transform = "translateX(100px)";
-//  console.log(frise);
-// }
+let moins = document.querySelector(".moins");
+let plus = document.querySelector(".plus");
+let nombrePlaceInput = document.querySelector(".number");
+let prixTotalInput = document.querySelector(".total");
+let nombrePlace = 0;
+let prixTotal = 0;
 
-// while (scrollPercentRounded >= 58 && scrollPercentRounded <= 80) {
 
-// }
 
-// let formulaireEnvoie =  document.querySelector('.envoie');
-// formulaireEnvoie.addEventListener("click", function(e) {
-//     formulaireEnvoie.innerHTML = '<div class="btnFinCT"><a class="btnFin envoie" href="#">Payer</a></div><div><p>Votre message à bien été envoyé</p></div>';
-//   console.log(e);
-//   });
-//   console.log('.envoie');
+moins.addEventListener('click', function(e) {
+    if (nombrePlace != 0) {
+      nombrePlace --;
+      nombrePlaceInput.value = nombrePlace;
+      prixTotal -= 10;
+      prixTotalInput.value = prixTotal;
+    }
+});
 
-//  Attention : pour entrer le chiffre du nombre de place : Attention O ou +
-// Au clic sur le formulaire, ca ajoute une ligne en html disant : "Votre message a bien été envoyé."
-// // Au clic : .loupe:hover > input {
-//   width: 7vw;
-//   padding-left: 8%;
-// }
-// Faire que le bouton recherche devienne valide.
-// Demander à Kévin comment faire pour faire disparaitre le texte du formulaire du de la loupe quand on veut taper ou qu'on à taper.
+plus.addEventListener('click', function(e) {
+  nombrePlace ++;
+  nombrePlaceInput.value = nombrePlace;
+  prixTotal += 10;
+  prixTotalInput.value = prixTotal;
+});
 
-// let titre = document.querySelector('h1');
-// titre.innerHTML = '<h1>Pamplemousse</h1>';
-// console.log(titre);
